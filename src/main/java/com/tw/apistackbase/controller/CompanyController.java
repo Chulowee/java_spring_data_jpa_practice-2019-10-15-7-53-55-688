85236abd9a03bae5df45254cc534a9046b1b2597
GRAY JEANNE MEDINA (SHP-IRIS-ISD-OOCLL/MNL) <MEDINGR2@OOCL.COM>
2019-10-17 15:18:33 +0800

    [Gray & Chloe] added RequestParam for list()

--- src/main/java/com/tw/apistackbase/controller/CompanyController.java
+++ src/main/java/com/tw/apistackbase/controller/CompanyController.java
@@ -3,10 +3,14 @@ package com.tw.apistackbase.controller;
 import com.tw.apistackbase.core.Company;
 import com.tw.apistackbase.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import sun.nio.cs.Surrogate;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 @RestController
@@ -18,8 +22,10 @@ public class CompanyController {
 
     @GetMapping(value= "/all" , produces = {"application/json"})
     @ResponseStatus(code = HttpStatus.OK)
-    public Iterable<Company> list() {
-        return companyRepository.findAll();
+    public Iterable<Company> list(
+            @RequestParam(required = false , defaultValue = "0")  Integer page ,
+            @RequestParam(required = false , defaultValue = "5")Integer size ) {
+        return companyRepository.findAll(PageRequest.of(page,size, Sort.by("name").ascending()));
     }
 
     @GetMapping( produces = {"application/json"})
