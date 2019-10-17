9a212317c0c3e239781480c2cf1d4791b4ce5371
GRAY JEANNE MEDINA (SHP-IRIS-ISD-OOCLL/MNL) <MEDINGR2@OOCL.COM>
2019-10-17 15:58:03 +0800

    [Gray & Chloe] added service layer

--- src/main/java/com/tw/apistackbase/controller/CompanyController.java
+++ src/main/java/com/tw/apistackbase/controller/CompanyController.java
@@ -2,6 +2,8 @@ package com.tw.apistackbase.controller;
 
 import com.tw.apistackbase.core.Company;
 import com.tw.apistackbase.repository.CompanyRepository;
+import com.tw.apistackbase.service.CompanyService;
+import jdk.internal.module.ModuleBootstrap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -16,23 +18,23 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
-    @Autowired
-    private CompanyRepository companyRepository;
 
+    @Autowired
+    private CompanyService companyService;
 
     @GetMapping(value= "/all" , produces = {"application/json"})
     @ResponseStatus(code = HttpStatus.OK)
     public Iterable<Company> list(
-            @RequestParam(required = false , defaultValue = "1")  Integer page ,
+            @RequestParam(required = false , defaultValue = "0")  Integer page ,
             @RequestParam(required = false , defaultValue = "5")Integer size ) {
-        return companyRepository.findAll(PageRequest.of(page,size, Sort.by("name").ascending()));
+        return companyService.findAll(PageRequest.of(page,size, Sort.by("name").ascending()));
     }
 
     @GetMapping( produces = {"application/json"})
     @ResponseStatus(code = HttpStatus.OK)
     public ResponseEntity<Optional<Company>> getCompanyByCompanyName
             (@RequestParam(required = false , defaultValue = "") String name) {
-        Optional<Company> fetchedCompany = companyRepository.findByNameContaining(name);
+        Optional<Company> fetchedCompany = companyService.findByNameContaining(name);
         if(fetchedCompany.isPresent()){
             return new ResponseEntity<>(fetchedCompany, HttpStatus.OK);
         }
@@ -42,14 +44,14 @@ public class CompanyController {
     @PostMapping(produces = {"application/json"})
     @ResponseStatus(code = HttpStatus.CREATED)
     public Company add(@RequestBody Company company) {
-        return companyRepository.save(company)  ;
+        return companyService.save(company)  ;
     }
 
     @DeleteMapping(value = "/{id}", produces = {"application/json"})
     public ResponseEntity<Optional<Company>> deleteCompanyByCompanyID (@PathVariable Long id) {
-        Optional<Company> fetchedCompany = companyRepository.findById(id);
+        Optional<Company> fetchedCompany = companyService.findById(id);
         if(fetchedCompany.isPresent()){
-            companyRepository.deleteById(id);
+            companyService.deleteById(id);
             return new ResponseEntity<>(fetchedCompany, HttpStatus.OK);
         }
         return  new ResponseEntity<>( HttpStatus.NOT_FOUND);
@@ -57,11 +59,11 @@ public class CompanyController {
 
     @PatchMapping(value = "/{id}", produces = {"application/json"})
     public ResponseEntity<Company> update(@PathVariable Long id, @RequestBody Company company) {
-        Optional<Company> companyToUpdate = companyRepository.findById(id);
+        Optional<Company> companyToUpdate = companyService.findById(id);
         if (companyToUpdate.isPresent()) {
             Company modifyCompany = companyToUpdate.get();
             modifyCompany.setName(company.getName());
-            Company savedCompany = companyRepository.save(modifyCompany);
+            Company savedCompany = companyService.save(modifyCompany);
             return new ResponseEntity<>(savedCompany, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
---
+++ src/main/java/com/tw/apistackbase/service/CompanyService.java
@@ -0,0 +1,37 @@
+package com.tw.apistackbase.service;
+
+import com.tw.apistackbase.core.Company;
+import com.tw.apistackbase.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CompanyService {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    public Iterable<Company> findAll(PageRequest name) {
+        return companyRepository.findAll();
+    }
+
+
+    public Optional<Company> findByNameContaining(String name) {
+        return companyRepository.findByNameContaining(name);
+    }
+
+    public Company save(Company company) {
+        return companyRepository.save(company);
+    }
+
+    public Optional<Company> findById(Long id) {
+        return companyRepository.findById(id);
+    }
+
+    public void deleteById(Long id) {
+         companyRepository.deleteById(id);
+    }
+}
