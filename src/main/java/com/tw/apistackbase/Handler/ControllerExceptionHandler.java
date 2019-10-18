c7dd5a92f077ed187dd71437c274dc02a358417b
chulowee <katrina.chloe.reyes@oocl.com>
2019-10-18 13:38:39 +0800

    Added Custom Exception

--- src/main/java/com/tw/apistackbase/Handler/ControllerExceptionHandler.java
+++ src/main/java/com/tw/apistackbase/Handler/ControllerExceptionHandler.java
@@ -1,16 +1,25 @@
 package com.tw.apistackbase.Handler;
 
+import com.tw.apistackbase.error.CustomError;
 import javassist.NotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class ControllerExceptionHandler extends Exception {
+public class ControllerExceptionHandler {
 
-    @ExceptionHandler
+    @ExceptionHandler(NotFoundException.class)
     @ResponseBody
-    public String handleNotFoundException(NotFoundException e) {
-        return e.getMessage();
+    @ResponseStatus(code = HttpStatus.NOT_FOUND)
+    public CustomError handleNotFoundException(NotFoundException e) {
+        CustomError customError = new CustomError();
+        customError.setCode(404);
+        customError.setMessage(e.getMessage());
+        return customError;
     }
+
+
 }
